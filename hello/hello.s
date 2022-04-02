ac7361b9a8948d8135ab7324ce6dae03684477f4
John Scott <johnrscott0@gmail.com>
2022-04-02 19:35:58 +0100

    Got the hello example working -- intel syntax needs an offset keyword to get the address

--- hello/hello.s
+++ hello/hello.s
@@ -1,15 +1,26 @@
 	/* Set Intel conventions for operand order and register naming */
 	.intel_syntax noprefix
 
-	.text
-	.global hello
-hello:
+	.data
+msg:
+	.ascii "Hello\n"
 	
+	.text
+	.global _start
+_start:
+	call print
 	jmp exit
-
-
+	
+print:
+	mov edx,7		/* message length */
+	mov ecx, offset msg		/* message to write */
+	mov ebx,1		/* file descriptor (stdout) */
+	mov eax,4		/* system call number (sys_write) */
+	int 0x80
+	ret
 	
 	/* Return control to the operating system */
 exit:
+	mov ebx, 0
 	mov eax, 1
 	int 0x80
