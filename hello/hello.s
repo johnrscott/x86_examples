e65c20e80061c5d4137c2342da9cd9905a2c9585
John Scott <johnrscott0@gmail.com>
2022-04-02 20:17:46 +0100

    Added some comments to hello example

--- hello/hello.s
+++ hello/hello.s
@@ -1,25 +1,37 @@
 	/* Set Intel conventions for operand order and register naming */
 	.intel_syntax noprefix
 
+	/* Start the data section, for storing a string to print */
 	.data
 msg:
 	.ascii "Hello\n"
-	
+	.equ msg_len, .-msg 
+
+	/* Start the text section (for code) */
 	.text
+
+	/* Make the entry point accessible to the linker. */
 	.global _start
+
+	/* Define the entry point for the program. The name is fixed
+	by a convention so that the linker can find it.
+ 	*/ 
 _start:
 	call print
 	jmp exit
-	
+
+	/* Print a string by calling the write() system call
+	*/
 print:
-	mov edx,7		/* message length */
+	mov edx, msg_len		/* message length */
 	mov ecx, offset msg		/* message to write */
 	mov ebx,1		/* file descriptor (stdout) */
 	mov eax,4		/* system call number (sys_write) */
 	int 0x80
 	ret
 	
-	/* Return control to the operating system */
+	/* Return control to the operating system, by calling
+	the exit() system call. */
 exit:
 	mov ebx, 0
 	mov eax, 1
