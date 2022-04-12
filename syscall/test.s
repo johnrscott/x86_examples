e111002c18d812b19bd0b9bda3190804e871d151
John Scott <johnrscott0@gmail.com>
2022-04-12 10:13:51 +0100

    Added asm test of read/write

--- syscall/test.s
+++ syscall/test.s
@@ -1,15 +1,32 @@
 	.include "syscall.inc"
 
 	.data
-buf:	.ascii "Write some text and press enter: "
+buf1:	.ascii "Write some text and press enter: "
+buf2:	.ascii "You wrote: "
+buf3:	.space 20
 	
 	.text
 	.global _start
 _start:
 	/* write(STDOUT, "Write some text and press enter: ", 33); */
-	mov $STDIN, %rdi
-	mov $buf, %rsi
+	mov $STDOUT, %rdi
+	mov $buf1, %rsi
 	mov $33, %rdx
+	call write
+	/* int count = read(STDIN, &buf3, 20); */
+	mov $STDIN, %rdi
+	mov $buf3, %rsi	
+	call read
+	mov %rax, %r12 /* Save count in r12, preserved across calls */
+	/* write(STDOUT, "You wrote: ", 11); */
+	mov $STDOUT, %rdi
+	mov $buf2, %rsi
+	mov $11, %rdx
+	call write
+	/* write(STDOUT, $buf3 , count); */
+	mov $STDOUT, %rdi
+	mov $buf3, %rsi
+	mov %r12, %rdx
 	call write
 	/* exit(2); */
 	mov $2, %rdi
